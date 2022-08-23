fab9601296f378f6db04f662b3c048cbf61b77fa
premchhabria <premvc702@gmail.com>
2022-08-23 11:39:48 +0530

    Initial maven payments v1 added

---
+++ Payments/maven/com-payments/src/main/java/com/payments/App.java
@@ -0,0 +1,17 @@
+package com.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("App starting...");
+		SpringApplication.run(App.class, args);
+		System.out.println("App started.");
+		
+	}
+
+}
